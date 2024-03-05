3d4dff643e4f9f50f4c29aaaeef3380ebd552a08
Your Name <corsiferrao@gmail.com>
2024-03-05 14:07:07 -0300

    enamble time slice + default head

--- main/main.c
+++ main/main.c
@@ -1,19 +1,20 @@
 /*
  * LED blink with FreeRTOS
  */
-
 #include <FreeRTOS.h>
 #include <task.h>
-#include <stdio.h>
+#include <semphr.h>
+#include <queue.h>
+
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 struct led_task_arg {
     int gpio;
     int delay;
 };
 
-void led_task(void *p)
-{
+void led_task(void *p) {
     struct led_task_arg *a = (struct led_task_arg *)p;
 
     gpio_init(a->gpio);
@@ -26,18 +27,17 @@ void led_task(void *p)
     }
 }
 
-int main()
-{
+int main() {
     stdio_init_all();
     printf("Start LED blink\n");
 
-    struct led_task_arg arg1 = { 15, 100 };
+    struct led_task_arg arg1 = {20, 100};
     xTaskCreate(led_task, "LED_Task 1", 256, &arg1, 1, NULL);
 
-    struct led_task_arg arg2 = { 14, 200 };
+    struct led_task_arg arg2 = {21, 200};
     xTaskCreate(led_task, "LED_Task 2", 256, &arg2, 1, NULL);
 
-    struct led_task_arg arg3 = { 13, 300 };
+    struct led_task_arg arg3 = {22, 300};
     xTaskCreate(led_task, "LED_Task 3", 256, &arg3, 1, NULL);
 
     vTaskStartScheduler();
