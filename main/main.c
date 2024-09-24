73e3795335aec020302fa2ade71abb11a5a8108a
danielmpinto <danielmessias.engenharia@gmail.com>
2024-09-24 22:49:53 +0000

    teste

--- main/main.c
+++ main/main.c
@@ -1,6 +1,3 @@
-/*
- * LED blink with FreeRTOS
- */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
@@ -9,86 +6,47 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
 #include "hardware/clocks.h"
 
+const int I2C_CHIP_ADDRESS_W = 0x42;
+const int I2C_CHIP_ADDRESS_R = 0x43;
+const int I2C_SDA_GPIO = 16;
+const int I2C_SCL_GPIO = 17;
 
+void i2c_task(void *p) {
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_GPIO);
+    gpio_pull_up(I2C_SCL_GPIO);
 
+    uint8_t buffer[6];
 
-volatile bool vsync_cam_flag;
-volatile bool href_cam_flag;
-volatile bool pclk_cam_flag;
+    // read whoami
+    uint8_t reg_address = 0x76;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS_W, &reg_address, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS_R, buffer, 1, false);
+    printf("WHOAMI: 0x%X \n", buffer[0]);
 
+    // TODO
+    // Leia o INT_ENABLE e imprima o valor
+    printf("INT_ENABLE: 0x%X \n", buffer[0]);
 
-
-struct cam_ov7670 {
-    const int VSYNC;
-    const int HREF;
-    const int PCLK;
-};
-
-// defining camera pins struct
-struct cam_ov7670 camera = {16, 17, 18};
-
-void camera_callback(uint gpio, uint32_t events) {
-  // entra 
-  if (gpio == camera.VSYNC && events == 0x8){
-    vsync_cam_flag = true;
-  }
-  if (gpio == camera.VSYNC && events == 0x4){
-    vsync_cam_flag = false;
-  }
-
-  if (gpio == camera.HREF && events == 0x8){
-    href_cam_flag = true;
-  }
-  if (gpio == camera.HREF && events == 0x4){
-    href_cam_flag = false;
-  }
-}
-
-void sync(void *p){
-    struct cam_ov7670 *pins = (struct cam_ov7670 *)p;
-    gpio_init(pins->VSYNC);
-    gpio_init(pins->HREF);
-    gpio_init(pins->PCLK);
-    gpio_set_dir(pins->VSYNC, GPIO_IN);
-    gpio_set_dir(pins->HREF, GPIO_IN);
-    gpio_set_dir(pins->PCLK, GPIO_IN);
-    while (true){
-        // starting a frame
-        while (!vsync_cam_flag);
-        while (vsync_cam_flag);
-        // preparing camera -> zering FIFO
-        // camera.preparing()
-        while (!href_cam_flag);
-        while(!pclk_cam_flag);
-        // starting capture to buffer
-        // exemplo qqvgaframe1[i] = LPC_GPIO2->FIOPIN; //Lê o barramento de dados do Ov7670
-        while (pclk_cam_flag); //Aguarda o sinal de PCLK ir a nível baixo
-
-
-        while (!vsync_cam_flag);
-        // stop capturing
-
-        }
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
 }
 
 int main() {
     stdio_init_all();
-
-    // Initializes clock on GP21 for OV7670
+    printf("Start RTOS \n");
     clock_gpio_init(21, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 10);
 
-    
-
-    // set callback for the end of the package
-    gpio_set_irq_enabled_with_callback(camera.VSYNC, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &camera_callback);
-
-    
-    xTaskCreate(sync, "Sync Task", 4096, &camera, 1, NULL);
-
+    xTaskCreate(i2c_task, "i2c task", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
 
-    while (true)
-        ;
+    while (true) {
+    }
 }
